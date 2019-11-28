d008e7ae669d87e9c4108e478848cf43fd9be322
Joanilza Bockhorny <jbockhorny@gmail.com>
2019-11-28 17:18:31 -0300

    inicio do teste

--- cli.js
+++ cli.js
@@ -1,4 +1,8 @@
-const {mdLinks} = require('./lib/index.js');
+#!/usr/bin/env node
+
+const mdLinks = require('./lib/index.js');
 
 const path = process.argv.slice(2).pop();
 mdLinks(path)
+.then((response)=> console.log(response))
+.catch((err)=> console.log(err))
