7881ddf71ad4d2e52281cb7dbb54d5728662c3df
Joanilza Bockhorny <jbockhorny@gmail.com>
2019-12-03 11:53:30 -0300

    Alteração no nome

--- cli.js
+++ cli.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
 const mdLinks = require('./lib/index.js');
-
 const path = process.argv.slice(2).pop();
+
 mdLinks(path)
-.then((response)=> console.log(response))
-.catch((err)=> console.log(err))
+  .then((response) => console.log(response))
+  .catch((err) => console.log(err))
